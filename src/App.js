ce2eb5468af8258cd038d7c7317da13ef6d08ebf
ZachGrande <zach.grande23@gmail.com>
2020-12-02 20:33:53 -0800

    Prepared workspace and initialized React state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-
+import React, { useState } from 'react';
 
 function App() {
+  // This is the state of the app, where user journal entries are maintained
+  // An entry has several components:
+  // - a title
+  // - a date
+  // - a description
+  // - a mood rating
+  // - a song
+  const [entries, modifyEntries] = useState();
   return (
     <div className="App">
       <h1>Start of project!</h1>
