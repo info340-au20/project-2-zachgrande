585e36acee7973abbfa2d43d4df3a420243fd2fe
ZachGrande <zach.grande23@gmail.com>
2020-12-10 09:55:37 -0800

    Minor styling changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import NavigationBar from './NavigationBar.js';
 import AboutPage from './AboutUs.js';
 import Form from './Form.js';
 import JournalLog from './JournalLog.js';
-//import Form from './Form.js';
 //import { Button } from 'reactstrap';
 
 function App() {
@@ -49,26 +48,9 @@ function App() {
       <header className="page-header">
         <h1><Link to="/">SongNotes</Link></h1>
         <NavigationBar />
-        {/* <div>
-          <nav>
-            <ul>
-              <li>
-                <img 
-                  className="icon homePage" 
-                  role="button" 
-                  src="img/logotransparent.png" 
-                  alt="logo"
-                  onClick={handleNav}
-                />
-              </li>
-              <li>
-                <button className="btn btn-primary btn-lg landingPage" onClick={handleNav}>
-                  Make Entry
-               </button>
-              </li>
-            </ul>
-          </nav>
-         </div> */}
+      </header>
+
+      <main>
         <nav className="row">
           <div className="col-3">
             <Switch>
@@ -79,24 +61,12 @@ function App() {
             </Switch>
           </div>
         </nav>
-
-
-      </header>
-
-      <main>
-        {/*<JournalLog />
-        <LandingPage />
-        <AboutPage />*/}
-        {/* {pageDisplay} */}
-        {/* {console.log("This is what the state looks like right now:")} */}
-        {/* {console.log(entries)} */}
       </main>
 
       <footer>
         <div className="footer-copyright text-center py-3"> &copy; INFO 340 AA -
             {/* <span role="button" className="about-us" onClick={handleNav}> About Us</span> */}
           <NavLink exact to="/about-us" className="nav-link-1" activeClassName={"activeLink"}> About Us</NavLink>
-
         </div>
       </footer>
     </div>
