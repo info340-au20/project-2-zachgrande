b375f0aa3cd870f65565109bc3499286c886a20b
Rebecca Haynes <beccah.haynes@gmail.com>
2020-12-16 05:32:49 -1000

    trying to work out the error handling

--- src/Form copy.js
+++ src/Form copy.js
@@ -2,17 +2,25 @@ import fetchTrack from './Track.js';
 //import MoodSelect from './MoodSelect.js';
 import { useEffect, useState } from 'react';
 import { propTypes } from 'react-bootstrap/esm/Image';
+import { Link, withRouter } from 'react-router-dom';
+import { useHistory } from "react-router";
 import firebase from 'firebase/app';
 import 'firebase/database';
 // import { Spinner } from 'reactstrap';
 
 // Similar code is in Form.js, moved here to ensure global variables update
 function Form(prop) {
+
   const user = prop.currentUser;
-  // Establish our variables from the prop
-  let entries = prop.entries;
-  let modifyEntries = prop.modifyEntries;
-  let formValid = prop.formValid;
+
+  /*//pass mood from MoodSelect 
+  //MOVED to App.js
+  const [moodEntry, setMoodEntry] = useState();
+  const handleMoodInput = (e) => {
+    
+    setMoodEntry(e);
+  }
+  console.log(moodEntry);*/
 
   let entryObj = {
     inputTitle: '',
@@ -20,51 +28,28 @@ function Form(prop) {
     inputDescription: '',
     inputSong: '',
   }
- 
-  //base appearance of form without error checks  
-  function MakeForm() {
-    /*Object.keys(entryObj).map((string) => {
-      let keyName = string.replace("input", "");
-      return (
-        <div className={string}>
-          <label htmlFor={string}>Post Title</label>
-          <input type="text" className="form-control form-control-lg" id={string} aria-label={"Entry " + keyName} placeholder="What do you want to title this post?" onChange={handleInput} />
-        </div>
-      )
-    })*/
-    return (
-      <div>
-        <div className="form-group">
-          <label htmlFor="inputTitle">Post Title</label>
-          <input type="text" className="form-control form-control-lg" id="inputTitle" aria-label="Entry Title" placeholder="What do you want to title this post?" onChange={handleInput} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputDate">Day</label>
-          <input type="date" id="inputDate" className="form-control form-control-lg" aria-label="Date" required onChange={handleInput} />
-          <div id="dateFeedback" className="invalid-feedback"></div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputDescription">How was your day?</label>
-          <textarea className="form-control" id="inputDescription" rows="3" onChange={handleInput}></textarea>
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputSong">Search for Today's Song</label>
-          <span className="glyphicon glyphicon-search"></span>
-          <input className="form-control" type="text" id="inputSong" placeholder="Search" aria-label="Search" onChange={handleInput} />
-        </div>
-    </div>
-    )
-  }
-
-  
-  //const [entryInput, setEntryInput] = useState(entryObj)
+  const [entryInput, setEntryInput] = useState(entryObj)
   let handleInput = (event) => {
     const inputValue = event.target.value;
     const inputName = event.target.id;
     entryObj[inputName] = inputValue; 
   }
 
+  // Establish our variables from the prop
+  let entries = prop.entries;
+  let modifyEntries = prop.modifyEntries;
+  let formValid = prop.formValid;
 
+
+  // MOVED TO JOURNALLOG.JS
+  // useEffect(() => {
+  //   const entryRef = firebase.database().ref('Person A');
+  //   entryRef.on('value', (snapshot) => {
+  //     const theValue = snapshot.val(); // convert to Javascript value
+  //     console.log("Value of database has changed");
+  //     console.log(theValue);
+  //   })
+  // }, [])
 
   let errorMessage = '';
   let validation = '';
@@ -77,12 +62,12 @@ function Form(prop) {
   let handleSubmit = (event) => {
     event.preventDefault();
     
-    //setEntryInput(entryObj);
+    setEntryInput(entryObj);
     //console.log(Object.entries(entryInput));
 
     //error handling
     
-    Object.entries(entryObj).map((array) => {
+    Object.entries(entryInput).map((array) => {
       let key = array[0];
       let value = array[1];
       let keyName = key.replace("input", "").toLowerCase();
@@ -126,9 +111,9 @@ function Form(prop) {
       let songEntry = [searchResults.artworkUrl100, searchResults.artistName, searchResults.trackName];
 
       const newEntry = {
-        postTitle: entryObj.inputTitle,
-        date: entryObj.inputDate,
-        dayDescription: entryObj.inputDescription,
+        postTitle: entryInput.inputTitle,
+        date: entryInput.inputDate,
+        dayDescription: entryInput.inputDescription,
         moodRating: prop.mood,
         artwork: songEntry[0],
         artist: songEntry[1],
@@ -167,20 +152,50 @@ function Form(prop) {
     }
 
     // The final step of our form, ensure nothing is computed until fetch completes
-    fetchTrack(entryObj.inputSong, finalizeUserInput);
+    fetchTrack(entryInput.inputSong, finalizeUserInput);
 
   }
 
   return (
     <section id="landingPage">
+
       <form>
-        <MakeForm />
-        <button className={"btn btn-primary " + isDisabled} id="submit" onClick={handleSubmit} aria-disabled={error}>Done</button>
-        <div className="song-loading-spinner disabled"></div>
-        <div className="errors"></div>
+        <div className="form-group">
+          <label htmlFor="inputTitle">Post Title</label>
+          <input type="text" className={"form-control form-control-lg " + validation} id="inputTitle" aria-label="Entry Title" placeholder="What do you want to title this post?" onChange={handleInput} />
+          <div class={"titleError " + feedbackClasses}>{errorMessage}</div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputDate">Day</label>
+          <input type="date" id="inputDate" className={"form-control form-control-lg " + validation} aria-label="Date" required onChange={handleInput} />
+          <div id="dateFeedback" className="invalid-feedback"></div>
+          <div class={"dateError " + feedbackClasses}>{errorMessage}</div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputDescription">How was your day?</label>
+          <textarea className={"form-control " + validation} id="inputDescription" rows="3" onChange={handleInput}></textarea>
+          <div class={"descriptionError " + feedbackClasses}>{errorMessage}</div>
+        </div>
+        {/*<div className="form-group">
+          <p role="label">Today's Mood Rating</p>
+          <MoodSelect moodEntry={handleMoodInput}/>
+          <div id="moodFeedback" className="invalid-feedback"></div>
+        </div>*/}
+        <div className="form-group">
+          <label htmlFor="inputSong">Search for Today's Song</label>
+          <span className="glyphicon glyphicon-search"></span>
+          <input className={"form-control " + validation} type="text" id="inputSong" placeholder="Search" aria-label="Search" onChange={handleInput} />
+          <div class={"songError " + feedbackClasses}>{errorMessage}</div>
+        </div>
+  <button className={"btn btn-primary " + isDisabled} id="submit" onClick={handleSubmit} aria-disabled={error}>Done</button>
+  <div className="song-loading-spinner disabled">
+  </div>
+  <div className="errors">
+  </div>
       </form>
+
     </section>
   )
 }
 
-export default Form;
+export default withRouter(Form);
