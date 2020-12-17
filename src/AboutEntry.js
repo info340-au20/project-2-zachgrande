f83399dac9b52c95bf5e1d32de6f3fd3b871124f
gideon chia <gidchia@uw.edu>
2020-12-17 15:08:24 +0800

    css edits to AboutEntry page

--- src/AboutEntry.js
+++ src/AboutEntry.js
@@ -12,14 +12,50 @@ function AboutEntry(prop) {
     }
   }
   return (
-    <div className="card mb-4">
-      <div className={"card-header color " + entry.moodRating}>
-        <img className="today album-test" id="about-entry-img" src={entry.artwork} alt="album cover" />
-        <h2 className="entry-title">{entry.postTitle}</h2>
-        <p className="date">{entry.date}</p>
-        <p>{entry.dayDescription}</p>
+    // Jerray delete all this commented stuff!! 
+
+    // <div className="row">
+    //   <div className="card mb-4">
+    //     <div className={"card-header color " + entry.moodRating}>
+    //       <div className="col">
+    //         <img className="today album-test" id="about-entry-img" src={entry.artwork} alt="album cover" />
+    //       </div>
+    //     </div>
+    //     <div>
+    //       <h2 className="entry-title">{entry.postTitle}</h2>
+    //       <p className="date">{entry.date}</p>
+    //       <p>{entry.dayDescription}</p>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="container">
+      <div className="card mb-4">
+        <div className={"card-header color " + entry.moodRating}>
+          <div className="row">
+            <img className="today-album-test" id="about-entry-img" src={entry.artwork} alt="album cover" />
+            <div className="col">
+              <h2 className="entry-title">{entry.postTitle}</h2>
+              <div className="row">
+                <p className="date">{entry.date}</p>  
+              </div>
+            </div>
+            <div className="col">
+              <p className="song-title">Song Title: </p>
+              <p className="song">{entry.songTitle}</p>
+              <p className="artist-title">Artist Name: </p>
+              <p className="artist">{entry.artist}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <h3 className="description-title">Description</h3>
+          <p className="description">{entry.dayDescription}</p> 
+        </div>
       </div>
     </div>
+
   )
 }
 
